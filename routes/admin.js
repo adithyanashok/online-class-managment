21d1cfa205a7c4c7e38ec34592cfd6af48569e19
Adithyan Ashok <adithyanashok406@gmail.com>
2021-08-11 16:20:56 +0530

    add time table form created and router

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
 res.render('admin/admin', { admin: true });
 });
 
+router.get("/add-time-table", (req, res) => {
+  res.render('admin/add-time-table')
+})
+router.post('/add-time-table', (req, res) => {
+  console.log(req.body);
+})
 module.exports = router;
