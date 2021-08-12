270e46b458c180fd310a0c4335d1955c6d23026d
Adithyan Ashok <adithyanashok406@gmail.com>
2021-08-12 12:50:54 +0530

    Time Table Modified

--- routes/admin.js
+++ routes/admin.js
@@ -8,17 +8,17 @@ res.render('admin/admin', { admin: true });
 
 router.get("/view-time-table", (req, res) => {
   studentHelpers.getAllTimeTable().then((timetable) => {
-    res.render('admin/view-time-table', { timetable })
+    res.render('admin/view-time-table', { admin:true, timetable })
   })
   
 })
 router.get('/add-time-table', (req, res) => {
-  res.render('admin/add-time-table')
+  res.render('admin/add-time-table', { admin: true})
 })
 router.post('/add-time-table', (req, res) => {
   console.log(req.body);
   studentHelpers.addTimeTable(req.body, (result) => {
-    res.render('admin/add-time-table')
+    res.render('admin/add-time-table', {admin: true})
   })
 })
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var studentHelpers = require('../helpers/student-helpers')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('user/user', { admin:false});
 });
-
+router.get('/timetable', (req, res) => {
+  studentHelpers.getAllTimeTable().then((timetable) => {
+    res.render('user/timetable', { timetable })
+  })
+})
 module.exports = router;
