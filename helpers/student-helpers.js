d138d5283caddf9f106d9787003f73bae5649496
Adithyan Ashok <adithyanashok406@gmail.com>
2021-08-12 21:49:00 +0530

    Time Table Delete And updation Finished

--- helpers/student-helpers.js
+++ helpers/student-helpers.js
@@ -1,5 +1,6 @@
 var db = require('../config/connection')
 var collection = require('../config/collection')
+var objectId = require('mongodb').ObjectId
 module.exports={
 
     addTimeTable: (timeTable, callback) => {
@@ -14,6 +15,49 @@ module.exports={
            let timetable = await db.get().collection(collection.TIMETABLE_COLLECTION).find().toArray()
                resolve(timetable)
             
+        })
+    },
+    // Delete Time Table
+    deleteDay: (tableId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.TIMETABLE_COLLECTION).remove({_id: objectId(tableId)}).then((response) => {
+                console.log(response);
+                resolve(response)
+            })
+        })
+    },
+    // Get Time Table Details
+    getTimeTable: (tableId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.TIMETABLE_COLLECTION).findOne({_id: objectId(tableId)}).then((timeTable) => {
+                resolve(timeTable)
+            })
+        })
+    },
+    // Update Time Table
+    updateTimeTable: (tableId, tableDetails) => {
+        console.log("***"+tableId);
+        console.log("********");
+        console.log(tableDetails);
+        console.log("********");
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.TIMETABLE_COLLECTION)
+            .updateOne({_id: objectId(tableId)},
+            {
+                $set: {
+                    Day: tableDetails.Day,
+                    FirstPeriod: tableDetails.FirstPeriod,
+                    SecondPeriod: tableDetails.SecondPeriod,
+                    ThirdPeriod: tableDetails.ThirdPeriod,
+                    FourthPeriod: tableDetails.FourthPeriod,
+                    FifthPeriod: tableDetails.FifthPeriod,
+                    SixthPeriod: tableDetails.SixthPeriod,
+                    
+                }
+            }
+            ).then((response) => {
+                resolve()
+            })
         })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -5,10 +5,11 @@ var studentHelpers = require('../helpers/student-helpers')
 router.get('/', function(req, res, next) {
 res.render('admin/admin', { admin: true });
 });
-
+// Time Table Methods
 router.get("/view-time-table", (req, res) => {
   studentHelpers.getAllTimeTable().then((timetable) => {
     res.render('admin/view-time-table', { admin:true, timetable })
+    console.log(timetable);
   })
   
 })
@@ -18,7 +19,25 @@ router.get('/add-time-table', (req, res) => {
 router.post('/add-time-table', (req, res) => {
   console.log(req.body);
   studentHelpers.addTimeTable(req.body, (result) => {
-    res.render('admin/add-time-table', {admin: true})
+    res.render('admin/view-time-table', {admin: true})
+  })
+})
+router.get('/delete-day/:id', (req, res) => {
+  let tableId = req.params.id
+  studentHelpers.deleteDay(tableId).then((response) => {
+    res.render('admin/view-time-table')
+  })
+})
+
+router.get('/edit-day/:id', async(req, res) => {
+  let timeTable = await studentHelpers.getTimeTable(req.params.id)
+  console.log(timeTable);
+  res.render('admin/edit-day', {timeTable}) 
+})
+router.post('/edit-day/:id', (req, res) => {
+  studentHelpers.updateTimeTable(req.params.id, req.body).then(() => {
+    console.log(req.body);
+    res.redirect('/admin/view-time-table')
   })
 })
 module.exports = router;
